0288b68400a55c63bcc01c6d725c7419d5798335
Christopher Lake <chris@vhx.tv>
2015-06-16 23:27:14 -0700

    First commit

---
+++ js/chhhris.js
@@ -0,0 +1,19 @@
+/*
+ * The Chhhris Responsive Library
+ * Copyright 2015
+ * Free to use under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+*/
+
+function chrisCheesySmile (){
+  $('.js-head-turn').fadeOut( "slow", "linear" );
+  setTimeout(sayHey, 100);
+}
+
+function sayHey (){
+  $('.heyo').fadeIn( "slow", "linear" )
+}
+
+$( document ).ready(function() {
+    setTimeout(chrisCheesySmile, 3000);
+});
